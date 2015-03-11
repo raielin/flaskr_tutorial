860b327cc4da3625c2b0731d31c4e36849fdf609
raielin@gmail.com <raielin@gmail.com>
2015-03-11 14:54:25 -0400

    Fix typo.

--- flaskr.py
+++ flaskr.py
@@ -42,7 +42,7 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 def connect_db():
     """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.row
+    rv.row_factory = sqlite3.Row
     return rv
 
 # create function to initialize the database
