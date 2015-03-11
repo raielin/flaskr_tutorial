1f7f9e33aeaf448cba2a93c9406d2550da9697d4
raielin@gmail.com <raielin@gmail.com>
2015-03-11 13:14:09 -0400

    Update code to reflect latest version in repo: https://github.com/mitsuhiko/flask/blob/master/examples/flaskr/flaskr.py.

--- flaskr.py
+++ flaskr.py
@@ -1,58 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+    Flaskr
+    ~~~~~~
+    A microblog example application written as Flask tutorial with
+    Flask and sqlite3.
+    :copyright: (c) 2015 by Armin Ronacher.
+    :license: BSD, see LICENSE for more details.
+"""
+
 # FILE: flaskr/flaskr.py
 # REFERENCE: http://flask.pocoo.org/docs/0.10/tutorial/setup/#tutorial-setup
 # CMD: start up application with: `$ python flaskr.py`
 
 
 # all the imports
-import sqlite3
+import os
+from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
 from contextlib import closing
 
-# configuration
-# per tutorial: ok in small apps to drop config directly into module.
-# cleaner solution would be to create separate `.ini` or `.py` file and load that or import the values from there.
-DATABASE = '/tmp/flaskr.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
 # create flaskr application and initialize it with the config from the same file, in flaskr.py
 app = Flask(__name__)
-app.config.from_object(__name__)
+
+# configuration
+# load default config and override config from an environment variable
+    # per tutorial: ok in small apps to drop config directly into module.
+    # cleaner solution would be to create separate `.ini` or `.py` file and load that or import the values from there.
+app.config.update(dict(
+    DATABASE = '/tmp/flaskr.db'
+    DEBUG = True
+    SECRET_KEY = 'development key'
+    USERNAME = 'admin'
+    PASSWORD = 'default'
+))
+
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 # method to connect to specified database. can be used to open a connection on request and also from Python shell or a script.
 def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.row
+    return rv
 
 # create function to initialize the database
 def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+    """Initializes the database."""
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
 # Create database in Python shell:
     # >>> from flaskr import init_db
     # >>> init_db()
 
+@app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    init_db()
+    print('Initialized the database.')
+
 # Initialize DB connections before each request & shut them down afterwards.
 # before_request() is called before a request and passed no arguments.
-@app.before_request
-def before_request():
-    g.db = connect_db()
+# @app.before_request
+# def before_request():
+#     g.db = connect_db()
 
-# teardown_request() gets called after the response has been constructed.
-@app.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-      db.close()
+def get_db():
+    """Opens a new database connection if there is none yet for the current application context."""
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+# teardown_appcontext() gets called after the response has been constructed.
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+      g.sqlite_db.close()
 
 # show_entries() view function passes entries as dicts to the show_entries.html template and returns the rendered one.
 @app.route('/')
 def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
@@ -62,9 +94,10 @@ def add_entry():
     if not session.get('logged_in'):
         abort(401)
     # use question marks when building SQL statements, otherwise will be vulnerable to SQL injection
-    g.db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
-    g.db.commit()
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+               [request.form['title'], request.form['text']])
+    db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
@@ -91,8 +124,8 @@ def logout():
     return redirect(url_for('show_entries'))
 
 # check to fire up server if we want to run this file as a standalone application
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
 
 
 # -------------------------------------
